807931f16e5154e32d0415d4bb4522219075f7a7
ABHIKRIT RAJ SINGH <56408635+abhikrit1212@users.noreply.github.com>
2022-10-30 17:26:04 +0530

    Added Special.py

---
+++ SpecialVariable__name__.py
@@ -0,0 +1,6 @@
+print(__name__)
+#It prints __main__.
+#It is used for the smae thing as in other languages i.e. from where we want to start our program.Main is starting point of execeution.
+#The first module name is __main__ always.
+#We can even consider module as a program.
+#The value of __name__ changes accordingly i.e if a program is stand alone program then it prints __main__ otherwise it prints module name.
